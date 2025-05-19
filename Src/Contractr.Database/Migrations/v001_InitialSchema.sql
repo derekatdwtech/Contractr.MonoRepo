b67d347abc2f6b18a32f3e62b291047bd76697f4
Derek Williams <derekcwilliams@protonmail.com>
2025-05-19 14:32:55 -0400

    fix: Database schema resizing.

--- Src/Contractr.Database/Migrations/v001_InitialSchema.sql
+++ Src/Contractr.Database/Migrations/v001_InitialSchema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users
 (
-    id VARCHAR(16) NOT NULL PRIMARY KEY,
+    id VARCHAR(100) NOT NULL PRIMARY KEY,
     first_name VARCHAR(50) NULL,
     last_name VARCHAR(50) NULL,
     email VARCHAR(254) NOT NULL,
@@ -15,7 +15,7 @@ CREATE TABLE organization
     city VARCHAR(168) NOT NULL,
     state VARCHAR(100) NOT NULL,
     country VARCHAR (30) NOT NULL,
-    owner VARCHAR(16) NOT NULL,
+    owner VARCHAR(100) NOT NULL,
     zip INT NOT NULL,
     phone VARCHAR(20) NOT NULL,
     parent_organization VARCHAR(16) NULL,
@@ -30,7 +30,7 @@ CREATE TABLE roles
 
 CREATE TABLE assigned_roles
 (
-    user_id VARCHAR(16) NOT NULL,
+    user_id VARCHAR(100) NOT NULL,
     member_of VARCHAR(16) NULL,
     role_id UNIQUEIDENTIFIER NOT NULL,
     CONSTRAINT fk_assigned_roles_user_id FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
@@ -61,17 +61,17 @@ CREATE TABLE deals
 CREATE TABLE deal_roles
 (
     deal_id VARCHAR(16) NOT NULL REFERENCES deals(id),
-    user_id VARCHAR(16) NOT NULL REFERENCES users(id)
+    user_id VARCHAR(100) NOT NULL REFERENCES users(id)
 );
 
 CREATE TABLE tasks (
     id VARCHAR(16) NOT NULL PRIMARY KEY,
     deal_id VARCHAR(16) NOT NULL REFERENCES deals(id),
     created_on DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    created_by VARCHAR(16) NOT NULL REFERENCES users(id),
+    created_by VARCHAR(100) NOT NULL REFERENCES users(id),
     title VARCHAR(200) NOT NULL,
     description VARCHAR(MAX) NULL,
-    assigned_to VARCHAR(16) NULL REFERENCES users(id),
+    assigned_to VARCHAR(100) NULL REFERENCES users(id),
     due_date DATETIME NULL,
     is_restricted BIT NOT NULL DEFAULT 0,
     status INT NOT NULL DEFAULT 1
@@ -86,12 +86,12 @@ CREATE TABLE task_history (
     task_id VARCHAR(16) NOT NULL REFERENCES tasks(id),
     action VARCHAR(100) NOT NULL,
     action_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    action_by VARCHAR(16) NOT NULL REFERENCES users(id)
+    action_by VARCHAR(100) NOT NULL REFERENCES users(id)
 )
 
 CREATE TABLE task_comments (
     task_id VARCHAR(16) REFERENCES tasks(id),
-    author VARCHAR(16) REFERENCES users(id),
+    author VARCHAR(100) REFERENCES users(id),
     body VARCHAR(MAX) NOT NULL,
     date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 )
@@ -136,7 +136,7 @@ CREATE TABLE signed_documents
     deal_id VARCHAR(16) NOT NULL,
     blob_uri VARCHAR(MAX) NOT NULL,
     file_name VARCHAR(MAX) NOT NULL,
-    signed_by VARCHAR(16) NOT NULL,
+    signed_by VARCHAR(100) NOT NULL,
     CONSTRAINT fk_signature_documents_signed_doc FOREIGN KEY (parent_document) REFERENCES signature_documents(id),
     CONSTRAINT fk_signature_documents_digned_by FOREIGN KEY (signed_by) REFERENCES users(id)
 
